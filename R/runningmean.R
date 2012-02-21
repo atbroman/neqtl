6330df58cad344dcf7885caf0b85b55c3943d850
Karl Broman <kbroman@gmail.com>
2012-02-21 10:48:41 -0600

    Remove the runningratio() function completely

--- R/runningmean.R
+++ R/runningmean.R
@@ -17,7 +17,7 @@
 #     A copy of the GNU General Public License, version 3, is available
 #     at http://www.r-project.org/Licenses/GPL-3
 #
-# Contains: runningmean, runningratio
+# Contains: runningmean
 ######################################################################
 
 ######################################################################
@@ -70,53 +70,5 @@ function(pos, value, at, window=1000, what=c("mean","sum", "median", "sd"))
 }
 
 
-######################################################################
-# runningratio
-#
-# take sum(numerator)/sum(denominator) in sliding window
-######################################################################
-runningratio <-
-function(pos, numerator, denominator, at, window=1000)
-{
-  n <- length(pos)
-  if(length(numerator) != n || length(denominator) != n)
-    stop("pos, numerator and denominator must all be the same length\n")
-
-  if(missing(at)) # if missing 'at', use input 'pos'
-    at <- pos
-
-  # check that pos is sorted
-  if(any(diff(pos) < 0)) { # needs to be sorted
-    o <- order(pos)
-    pos <- pos[o]
-    value <- value[o]
-  }
-    
-  # check that pos is sorted
-  if(any(diff(at) < 0)) { # needs to be sorted
-    o.at <- order(at)
-    at <- at[o.at]
-    reorderresult <- TRUE
-  }
-  else reorderresult <- FALSE
-    
-  n.res <- length(at)
-
-  z <- .C("R_runningratio",
-          as.integer(n),
-          as.double(pos),
-          as.double(numerator),
-          as.double(denominator),
-          as.integer(n.res),
-          as.double(at),
-          z=as.double(rep(0,n.res)),
-          as.double(window),
-          PACKAGE="neqtl")$z
-
-  if(reorderresult)
-    z <- z[match(1:length(z), o.at)]
-
-  z
-}
 
 # end of runningmean.R
