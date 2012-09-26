81ecdc72c10ddc38302fa39c390bb4da8058c1af
Aimee Teo Broman <abroman@biostat.wisc.edu>
2012-09-26 10:47:28 -0500

    in nullsims function, add random numbers to end of cross$pheno, rather than replace

--- R/nullsims.R
+++ R/nullsims.R
@@ -7,11 +7,12 @@ nullsims <- function(cross, n.sim=1000, n.batch=4, verbose=TRUE, ...){
   if(n.sim %% n.batch != 0)
     stop("Make batchsize a factor of n.sim.")
   batchsize <- n.sim/n.batch
+  np <- nphe(cross)
   results <- NULL
   for(i in 1:n.batch) {
     if(verbose) print(paste("batch",i))
-    cross$pheno[,1:batchsize] <- rnorm(batchsize*nind(cross))
-    out <- scanone(cross, pheno.col=1:batchsize, ...)
+    cross$pheno[,(1+np):(batchsize+np)] <- rnorm(batchsize*nind(cross))
+    out <- scanone(cross, pheno.col=(1+np):(batchsize+np), ...)
   results.batch <- apply(out[,-(1:2)],2,max,na.rm=TRUE)
   results <- c(results,results.batch)
   }
