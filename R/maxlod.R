6aadbb40fdf4eb583ce23f7c8f23820bbe6ad680
Aimee Teo Broman <abroman@biostat.wisc.edu>
2014-03-05 11:46:02 -0600

    Correction to maxlod() function to allow for only 1 lod column.

--- R/maxlod.R
+++ R/maxlod.R
@@ -3,11 +3,11 @@
 maxlod <- function(scan.out){
   chrs <- levels(scan.out[,1])
 # the maximum lod scores on each chromosome (output is dim nchr x ntraits)
-  maxlod <-  apply(scan.out[,-(1:2)], 2, tapply, scan.out[,1], max)
+  maxlod <-  apply(scan.out[,-(1:2), drop=FALSE], 2, tapply, scan.out[,1], max)
 # the locations (take average of those locations with the maximum LOD score)
 # [list by chromosome, each being a vector of peak positions
   maxlod.pos <- by(scan.out,scan.out$chr,function(x)
-       apply(x[,-(1:2)],2,function(a,b)
+       apply(x[,-(1:2),drop=FALSE],2,function(a,b)
        mean(b[!is.na(a) & a==max(a, na.rm=TRUE)]),x[,2]))
   attributes(maxlod.pos) <- list(names=
                  attributes(maxlod.pos)$dimnames[[1]])
