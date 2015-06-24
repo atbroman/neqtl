9ba0ee120f7e7a108315e8f78a079ae265b15810
Karl Broman <kbroman@gmail.com>
2015-06-24 06:57:29 -0500

    In nullsims(), need qtl::nphe() and qtl::nind()

--- R/nullsims.R
+++ R/nullsims.R
@@ -1,4 +1,3 @@
-
 ## Simulates the null distribution of max LOD score across genome ##
 ## ... are parameters to scanone() ##
 ## n.sim/n.batch ~ 250
@@ -7,11 +6,11 @@ nullsims <- function(cross, n.sim=1000, n.batch=4, verbose=TRUE, ...){
   if(n.sim %% n.batch != 0)
     stop("Make batchsize a factor of n.sim.")
   batchsize <- n.sim/n.batch
-  np <- nphe(cross)
+  np <- qtl::nphe(cross)
   results <- NULL
   for(i in 1:n.batch) {
     if(verbose) print(paste("batch",i))
-    cross$pheno[,(1+np):(batchsize+np)] <- rnorm(batchsize*nind(cross))
+    cross$pheno[,(1+np):(batchsize+np)] <- rnorm(batchsize*qtl::nind(cross))
     out <- scanone(cross, pheno.col=(1+np):(batchsize+np), ...)
   results.batch <- apply(out[,-(1:2)],2,max,na.rm=TRUE)
   results <- c(results,results.batch)
