3b12212a2557410f8fcdd54ea0978b7a159e2eed
Aimee Teo Broman <abroman@biostat.wisc.edu>
2017-02-09 10:35:13 -0600

    Added inc=0.2 parameter to neqtl functions

--- R/neqtl.R
+++ R/neqtl.R
@@ -1,14 +1,15 @@
 
-neqtl <- function(sigpos.out,chr,pos,win=5)
-     smoothall(sigpos.out,chr,pos,window=win)
+neqtl <- function(sigpos.out, chr, pos, win=5, inc=0.2)
+     smoothall(sigpos.out, chr, pos, win, inc)
 
 smoothall <-
-function(themax, thechr, thepos, window=5)
+function(themax, thechr, thepos, window, inc)
 {
   thesmooth <- vector("list", length(themax))
   names(thesmooth) <- names(themax)
   for(i in names(themax))
-      thesmooth[[i]] <- smoothchr(themax[[i]], thepos[thechr==i], window=window)
+      thesmooth[[i]] <- smoothchr(themax[[i]], thepos[thechr==i],
+                                  window, inc)
    out <- NULL
   for(i in 1:length(thesmooth))
     out <- rbind(out, data.frame(chr=rep(names(themax)[i], nrow(thesmooth[[i]])),
@@ -23,12 +24,12 @@ function(themax, thechr, thepos, window=5)
 ## Uses postions from thepos for smoothing: ATB 9/10/09 ##
 ## In theloc by=0.2 was outside the seq() function--moved it inside  ATB 12/15/09 ##
 ## removed  u <- unique(theloc), theloc already unique ATB 5/24/12 ##
+## added user-defined increment (inc) ATB 2/09/16 ##
 
 smoothchr <-
-function(themax, thepos, window=5)
+function(themax, thepos, window, inc)
 {
-  theloc <- sort(unique(c(thepos, seq(0, max(thepos), by=0.2))))
-
+  theloc <- sort(unique(c(thepos, seq(0, max(thepos), by=inc))))
   temploc <- c(themax, theloc)
   tempval <- c(rep(1, length(themax)), rep(0, length(theloc)))
   o <- order(temploc)
